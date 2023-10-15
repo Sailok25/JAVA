4dca9125ea9da3cce2c85ad9bc36088783d62d1c
Sailok <127998789+Sailok25@users.noreply.github.com>
2023-10-15 21:39:52 +0200

    Add files via upload

---
+++ Ejercicio1 - Hola Mundo.java
@@ -0,0 +1,8 @@
+public class holaMundo {
+    public static void main(String[] args) throws Exception {
+        /* Exercici 1:
+        Fem un prgroama en java on: 
+        impremeixi per pantalla un missatge: Hola mundo! */
+        System.out.println("Hola mundo!");
+    }
+}
---
+++ Ejercicio5 - While Contador.java
@@ -0,0 +1,10 @@
+public class whileContador {
+    public static void main(String[] args) {
+        /* Exercici 5:
+        Imprimeix per pantalla 5 cops el teu nom (utilitzant el bucle "while") */
+        int contador;
+        while (contador = 1; contador <= 5; contador++) {
+            System.out.println("Sailok");
+        }
+    }
+}
