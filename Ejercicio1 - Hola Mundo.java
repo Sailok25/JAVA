6eca653ff56a029b89e7fcfc2175e9f712b1c177
Sailok <127998789+Sailok25@users.noreply.github.com>
2023-10-16 20:45:20 +0200

    Delete Ejercicio1 - Hola Mundo.java

--- Ejercicio1 - Hola Mundo.java
+++
@@ -1,8 +0,0 @@
-public class holaMundo {
-    public static void main(String[] args) throws Exception {
-        /* Exercici 1:
-        Fem un prgroama en java on: 
-        impremeixi per pantalla un missatge: Hola mundo! */
-        System.out.println("Hola mundo!");
-    }
-}
